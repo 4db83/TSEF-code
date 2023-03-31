6c27846a27049c887c61f4c2a170f7f0b70b5fe4
4db83 <84711454+4db83@users.noreply.github.com>
2023-03-31 14:31:24 +0200

    add HP filter

--- plot_MA1_rho_beta_mapping.m
+++ plot_MA1_rho_beta_mapping.m
@@ -1,17 +1,14 @@
 % Script: plot_MA1_rho_beta_mapping.m
-% NOTE: To be able to run this code, you need the contents of the db.toolbox available from:
-% https://github.com/4db83/db.toolbox/archive/refs/heads/main.zip. Unzip the contents locally
-% to the same directory as this script, and then uncomment the following line below:
-% addpath(genpath('./db.toolbox-main'))
+addpath(genpath('D:/matlab.tools/db.toolbox/db/'))
 clear;clc;clf;
 
 % create inline function
 f = inline('b./(1+b.^2)','b');
-x = linspace(-3,3,100);
+x = linspace(-3,3,100); % grid where to evaluate the function
 
 % plot
 set(groot,'defaultLineLineWidth',1.5); 
-fns = 18;         % font size for plots
+fns = 17; % font size for plots
 
 hold on;
   plot(x,f(x))
@@ -19,18 +16,14 @@ hold on;
   vline(-1,'k--');vline(1,'k--')
   hline(0.4,'r-',[],[],'r');vline(0.5,'r-');vline(2,'r-')
 hold off;		
-box on; grid on;
-setplot([.84 .28], fns);
+setplot([.1 .44 .8 .28], 1, fns);
 setyticklabels([-.5:.1:.5], 1); 
-set(gca,'GridLineStyle',':','GridAlpha',1/3);
-tickshrink(.8)
-setoutsideTicks
-% setplot([.9 .6],13);
-
+setoutsideTicks; 
 xlabel('$\beta_1$','Interpreter','Latex')
 ylabel('$\rho(1)$','Interpreter','Latex')
-moveylabel(-.05);  
-movexlabel(-.05);  
+moveylabel(.05);  
+movexlabel(-.05);
+add2yaxislabel
 
-% uncomment to print to pdf 
-% print2pdf('plot_MA1_rho_beta_mapping','../graphics')
+% UNCOMMENT TO PRINT TO PDF 
+% print2pdf('plot_MA1_rho_beta_mapping', 1)
---
+++ plot_consumption_income.m
@@ -0,0 +1,76 @@
+% plot consumption and disposable income data for the US to replicate Hamilton figure on page 601
+addpath(genpath('D:/matlab.tools/db.toolbox/db/'))
+clear;clc;clf;
+
+% get data from FREDwebsite if not in ./data/ directory
+usdat = as_timetable(getFredData('pcecc96', '1947-01-01', '2021-12-31','lin','q'),'pcecc96');
+% usdat = synchronize(usdat,as_timetable(getFredData('USRECQ', '1947-01-01', '2021-12-31','lin','q'),'NBER'));
+usdat = synchronize(usdat,as_timetable(getFredData('dpic96', '1947-01-01', '2021-12-31','lin','q'),'dpic96'));
+% combs = synchronize(us_data_merged,usdat);
+% save('./data/consumption_income_US_2021Q4.mat', 'usdat');
+
+% load consumption_income_US data for us
+load('./data/consumption_income_US_2021Q4.mat')
+clc;clf;
+usdat.c = log(usdat.pcecc96);
+usdat.y = log(usdat.dpic96);
+% head2tail(usdat);
+ss = timerange('Q1-1947', 'Q4-2021', 'closed');
+ss = timerange('Q1-1947', 'Q4-1989', 'closed');
+ss = timerange('Q1-1947', 'Q4-2019', 'closed');
+% extract variables of interst
+y = usdat(ss,'y');
+c = usdat(ss,'c');
+
+clf;
+set(groot,'defaultLineLineWidth',2);
+% font size
+Fns = 20;
+hold on;
+  plot(c.Variables);
+  plot(y.Variables);
+hold off;
+setplot([.57 .26]);
+setdateticks(usdat.Time,31,'yyyy', Fns);	
+setoutsideTicks; add2yaxislabel
+% add legend
+addlegend({'Consumption $(c_t)$','Disposible Income $(y_t)$'}, 1, Fns + 1, 'Latex')
+
+% UNCOMMENT TO PRINT TO PDF 
+% print2pdf('gpd_conusmption_2021Q4', 1);
+
+
+
+%  EOF 
+
+
+
+
+
+
+
+
+
+
+
+
+
+% aout = ols(c,y,'Consumption');
+% plot(aout.uhat)
+% nexttile
+% %%
+% t = tiledlayout(2,2,'TileSpacing','Compact');
+% 
+% % Tile 1
+% nexttile([1 2])
+% plot(aout.uhat)
+% 
+% % Tile 2
+% nexttile
+% plot(rand(1,20))
+% title('Sample 2')
+% 
+% % Tile 3
+% nexttile
+% plotacf(aout.uhat)
+% subplot(2,2,3:4)
